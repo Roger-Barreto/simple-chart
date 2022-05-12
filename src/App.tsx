a1f6845b1c567ec646332e57aece4d3a04af90e8
Roger Barreto <roger@clubeagro.com.br>
2022-05-12 15:33:22 -0300

    feat: header + background styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import * as Highcharts from 'highcharts';
+import Exporting from 'highcharts/modules/exporting';
+
 
 function App() {
+  Exporting(Highcharts);
+  const userName = "John Doe";
+  const screenName = "Sales Report";
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="AppHeader">
+        <div className="Container">
+          <h2 className="User">
+            <div className="Circle"></div>
+            {userName}
+            </h2>
+          <h1 className="Title">{screenName}</h1>
+        </div>
       </header>
     </div>
   );
